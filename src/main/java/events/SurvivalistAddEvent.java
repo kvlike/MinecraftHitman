e3f4552e7c158c01e9de4469e3c3359f9ed6d507
Mikołaj Kowalik <74322367+kvlike@users.noreply.github.com>
2021-03-25 08:54:36 +0100

    Create SurvivalistAddEvent.java

---
+++ src/main/java/events/SurvivalistAddEvent.java
@@ -0,0 +1,29 @@
+package events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class SurvivalistAddEvent extends Event {
+
+    private Player player;
+
+    public SurvivalistAddEvent(Player player){
+        this.player = player;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+}
